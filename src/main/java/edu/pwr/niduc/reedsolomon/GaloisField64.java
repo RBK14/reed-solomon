665f2347918501845c121334b7906d2c42b26236
RBK14 <maciek.rybka2004@gmail.com>
2024-11-16 02:58:36 +0100

    Addition and multiplication return an alpha value.

--- src/main/java/edu/pwr/niduc/reedsolomon/GaloisField64.java
+++ src/main/java/edu/pwr/niduc/reedsolomon/GaloisField64.java
@@ -6,9 +6,8 @@ import java.util.*;
 
 public class GaloisField64 {
 
-    private static final int GF_POWER = 6;                       // m = 6
-    private static final int GF_SIZE = 64;                       // q = 64
-    private static final int[] PRIMITIVE_POLY = {1,1,0,0,0,0,1}; // p(x) = x^6 + x + 1
+    private static final int m = 6;                                         // m = 6
+    private static final int q = (int) Math.pow(2,m);                       // q = 64
 
     private final Map<Integer, int[]> galoisFieldElements;
 
@@ -18,62 +17,76 @@ public class GaloisField64 {
 
     private Map<Integer, int[]> generateGaloisFieldElements() {
         Map<Integer, int[]> elements = new HashMap<>();
-        List<Integer> sequence = new ArrayList<>(Arrays.asList(1,0,0,0,0,0));
+        List<Integer> sequence = new ArrayList<>(Arrays.asList(1,0,0));
 
         // Generowanie sekwencji
-        for (int i = 0; i < GF_SIZE; i++) {
+        for (int i = 0; i < q; i++) {
             sequence.add(sequence.get(i) ^ sequence.get(i+1));
         }
 
         // Odczytanie postaci wektorowej elementów z sekwencji pseudolosowej
-        for (int alpha = 0; alpha < GF_SIZE - 1; alpha++) {
-            List<Integer> subSequence = sequence.subList(alpha, alpha + GF_POWER);
+        for (int alpha = 0; alpha < q - 1; alpha++) {
+            List<Integer> subSequence = sequence.subList(alpha, alpha + m);
             int[] subSequenceArray = subSequence.stream().mapToInt(Integer::intValue).toArray();
             elements.put(alpha, subSequenceArray);
         }
 
         // Dodanie wektora zerowego na ostatniej pozycji możliwej pozycji (GF_SIZE)
-        elements.put(GF_SIZE, new int[GF_POWER]);
+        elements.put(q, new int[m]);
 
         return elements;
     }
 
-    public int[] multiply(int alpha1, int alpha2) {
+    public int multiply(int alpha1, int alpha2) {
         // Sprawdzanie, wielomiany są elementami pola Galois
-        if (alpha1 > GF_SIZE - 2 && alpha1 != GF_SIZE) {
-            throw new OutOfGaloisFieldException("Alpha^" + alpha1 + " is not Galois Field (2" + GF_POWER + ") element");
+        if (alpha1 > q - 2 && alpha1 != q) {
+            throw new OutOfGaloisFieldException("Alpha^" + alpha1 + " is not Galois Field (2" + m + ") element");
         }
-        if (alpha2 > GF_SIZE - 2 && alpha2 != GF_SIZE) {
-            throw new OutOfGaloisFieldException("Alpha^" + alpha2 + " is not Galois Field (2" + GF_POWER + ") element");
+        if (alpha2 > q - 2 && alpha2 != q) {
+            throw new OutOfGaloisFieldException("Alpha^" + alpha2 + " is not Galois Field (2" + m + ") element");
         }
 
         // Mnożenie przez 0
-        if (alpha1 == GF_SIZE || alpha2 == GF_SIZE) return new int[GF_POWER];
+        if (alpha1 == q || alpha2 == q) return q;
 
         // Mnożenie postaci multiplikatywnych wielomianów
-        int result = (alpha1 + alpha2) % (GF_SIZE - 1);
-
-        return galoisFieldElements.get(result);
+        return (alpha1 + alpha2) % (q - 1);
     }
 
-    public int[] add(int[] poly1, int[] poly2) {
-        // Sprawdzanie, czy wielomiany są elementami pola Galois
-        if (poly1.length > GF_POWER || poly2.length > GF_POWER) {
-            throw new OutOfGaloisFieldException("One of the polynomials is not Galois Field (2" + GF_POWER + ") element");
-        }
+    public int add(int alpha1, int alpha2) {
+        // TODO: Sprawdzanie, czy wielomiany są elementami pola Galois
 
-        int[] result = new int[GF_POWER];
+        int[] vector1 = galoisFieldElements.get(alpha1);
+        int[] vector2 = galoisFieldElements.get(alpha2);
+
+        int[] result = new int[m];
 
         // Dodawanie cyfr na odpowiadających pozycjach
-        for (int i = 0; i < Math.max(poly1.length, poly2.length); i++) {
-            if (i > poly1.length - 1) {
-                result[i] = poly2[i];
-            } else if (i > poly2.length - 1) {
-                result[i] = poly1[i];
+        for (int i = 0; i < Math.max(vector1.length, vector2.length); i++) {
+            if (i > vector1.length - 1) {
+                result[i] = vector2[i];
+            } else if (i > vector2.length - 1) {
+                result[i] = vector1[i];
             } else {
-                result[i] = poly1[i] + poly2[i];
+                result[i] = vector1[i] ^ vector2[i];
             }
         }
-        return result;
+        return convertToMultiplicative(result);
+    }
+
+    private int convertToMultiplicative(int[] vector) {
+        int alpha = -1;
+
+        for (Map.Entry<Integer, int[]> entry : galoisFieldElements.entrySet()) {
+            if (Arrays.equals(entry.getValue(), vector)) {
+                alpha = entry.getKey();
+                break;
+            }
+        }
+
+        if (alpha == -1) {
+            throw new RuntimeException("Cannot find alpha");
+        }
+        return alpha;
     }
 }
