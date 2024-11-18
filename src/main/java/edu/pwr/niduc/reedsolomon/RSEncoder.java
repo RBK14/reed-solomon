38354b467456c2c77ccefc8bcf8fcc291494be18
RBK14 <maciek.rybka2004@gmail.com>
2024-11-18 20:26:02 +0100

    Methods converting message to polynomial.

--- src/main/java/edu/pwr/niduc/reedsolomon/RSEncoder.java
+++ src/main/java/edu/pwr/niduc/reedsolomon/RSEncoder.java
@@ -1,47 +1,114 @@
 package edu.pwr.niduc.reedsolomon;
 
 import edu.pwr.niduc.util.InvalidCorrectionValueException;
+import edu.pwr.niduc.util.MessageTooLongException;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class RSEncoder {
 
+    private final int m;
     private final int t;
-    private final GaloisField64 galoisField64;
+    private final GaloisField galoisField;
     private final GeneratingPolynomial generatingPolynomial;
 
-    public RSEncoder(int t) {
+    public RSEncoder(int m, int t) {
+        this.m = m;
         this.t = t;
-        this.galoisField64 = new GaloisField64();
-        this.generatingPolynomial = new GeneratingPolynomial();
+        this.galoisField = new GaloisField(m);
+        this.generatingPolynomial = new GeneratingPolynomial(m);
     }
 
-    public int[] encode_message(int[] message) {
+    public int[] encodeMessage(int[] messagePolynomial) {
         if (t < 1) {
             throw new InvalidCorrectionValueException("Correction value t must be greater or equal to 1");
         }
 
         // Generowanie wielomianu generującego
-        int[] genPoly = generatingPolynomial.generatePolynomial(t);
+        int[] generatingPolynomial = this.generatingPolynomial.generatePolynomial(t);
 
         // Obliczanie n-k
-        int n = message.length + 2 * t; // n = k + r (r = 2t)
-        int k = message.length;
-        int nMinusK = n - k;
+        int n = messagePolynomial.length + 2 * t; // n = k + r (r = 2t)
+        int k = messagePolynomial.length;
+        int power = n - k;
 
         // Przesunięcie wiadomości przez mnożenie przez x^(n-k)
-        int[] shiftedMessage = new int[message.length + nMinusK];
-        System.arraycopy(message, 0, shiftedMessage, 0, message.length);
+        int[] shiftedMessage = new int[messagePolynomial.length + power];
+        System.arraycopy(messagePolynomial, 0, shiftedMessage, 0, messagePolynomial.length);
 
         // Obliczenie reszty (r(x))
-        int[] remainder = galoisField64.dividePolynomials(shiftedMessage, genPoly);
+        int[] remainder = galoisField.dividePolynomials(shiftedMessage, generatingPolynomial);
 
         // Połączenie wiadomości z resztą
-        return mergeArraysUsingStreams(message, remainder);
+        return mergeArraysUsingStreams(messagePolynomial, remainder);
     }
 
-    public static int[] mergeArraysUsingStreams(int[] arr1, int[] arr2) {
+    public int[] convertMessageToPolynomial(int message) {
+        // Obliczanie minimalnej liczby bitów potrzebnych do reprezentacji wiadomości
+        int bitLength = Integer.toBinaryString(message).length();
+        validateMessageLength(bitLength);
+
+        // Zaokrąglenie do najbliższej wielokrotności m
+        bitLength = ((bitLength + m-1) / m) * m;
+
+        // Wypełnienie tablicy bitami
+        int[] binaryArray = new int[bitLength];
+        for (int i = 0; i < bitLength; i++) {
+            binaryArray[i] = (message >> i) & 1;
+        }
+
+        // Zamiana reprezentacji bitowej wiadomości na wielomian o współczynnikach alfa
+        int[] polynomial = new int[bitLength / m];
+        for (int i = 0; i < polynomial.length; i++) {
+            int k = m*i;
+            int[] subArray = Arrays.copyOfRange(binaryArray, k, k+m);
+            polynomial[i] = galoisField.convertToMultiplicative(subArray);
+        }
+
+        return polynomial;
+    }
+
+    public int[] convertMessageToPolynomial(String message) {
+        // Przekształcenie wiadomości na binarną reprezentację całkowitą
+        StringBuilder binaryString = new StringBuilder();
+        for (char c : message.toCharArray()) {
+            String binaryChar = String.format("%8s", Integer.toBinaryString(c)).replace(' ', '0');
+            binaryString.append(binaryChar); // Dodanie każdego znaku w postaci binarnej
+        }
+
+        // Długość binarnej reprezentacji
+        int bitLength = binaryString.length();
+        validateMessageLength(bitLength);
+
+        // Zaokrąglenie do najbliższej wielokrotności m
+        bitLength = ((bitLength + m - 1) / m) * m;
+
+        // Wypełnienie tablicy bitowej, jeśli bitLength > binaryString.length(), reszta tablicy zostaje wypełniona zerami
+        int[] binaryArray = new int[bitLength];
+        for (int i = 0; i < binaryString.length(); i++) {
+            binaryArray[i] = binaryString.charAt(i) - '0'; // Konwersja char ('0' lub '1') na int
+        }
+
+        // Zamiana reprezentacji bitowej wiadomości na wielomian o współczynnikach alfa
+        int[] polynomial = new int[bitLength / m];
+        for (int i = 0; i < polynomial.length; i++) {
+            int k = m * i;
+            int[] subArray = Arrays.copyOfRange(binaryArray, k, k + m);
+            polynomial[i] = galoisField.convertToMultiplicative(subArray);
+        }
+
+        return polynomial;
+    }
+
+    private int[] mergeArraysUsingStreams(int[] arr1, int[] arr2) {
         return IntStream.concat(Arrays.stream(arr1), Arrays.stream(arr2)).toArray();
+    }
+
+    private void validateMessageLength(int bitLength) {
+        int maxLength = (((int) Math.pow(2,m) - 1) - 2*t) * m;
+        if (bitLength > maxLength) {
+            throw new MessageTooLongException("Message length must be less than or equal to " + maxLength);
+        }
     }
 }
